19bc8e94bf32c5cb95dc6b75240f2a8e00a5068f
Samantha King <samanthak2010@live.com>
2022-03-06 13:56:43 -0600

    CAC-102 Exercise 7

---
+++ exercises/exercise7.py
@@ -0,0 +1,37 @@
+# Exercise 7 Looping
+
+# 4-3
+def number():
+    for alpha in range(1, 21):
+        print(alpha)
+
+
+number()
+
+
+# 4-6
+def number2():
+    for beta in range(1, 21, 2):
+        print(beta)
+
+
+number2()
+
+
+# 4-7
+def number3():
+    for charlie in range(3, 31, 3):
+        print(charlie)
+
+
+number3()
+
+
+# 4-8
+def number4():
+    for delta in range(1, 11):
+        cube = delta ** 3
+        print(cube)
+
+
+number4()
